6d8183a5dbe4f1ce352975f284c5122eaa7030a1
EthanVV <ethan.vanvleck@gmail.com>
2018-11-27 14:19:36 -0600

    render duration in readable format

--- client/src/components/historyItem.jsx
+++ client/src/components/historyItem.jsx
@@ -5,21 +5,35 @@ const s = {
     backgroundColor: 'lightgrey',
   },
   name: {
-    fontSize: 24,
+    fontSize: 18,
+    fontStyle: 'bold',
+    color: 'Black'
+  },
+  duration: {
+    fontSize: 22,
     fontStyle: 'bold',
     color: 'Black'
   },
   time: {
     paddingLeft: '4px',
     color: 'Black',
-    fontSize: 18
+    fontSize: 14
   }
 }
 
 const HistoryItem = (props) => {
+  let dur = moment.duration(props.entry.timestamp_end - props.entry.timestamp_start);
+  let seconds = dur.seconds();
+  let minutes = dur.minutes();
+  let hours = dur.hours();
+
+
   return (
     <div style={{'backgroundColor': props.color, display: 'grid', gridTemplateRows: 'repeat(3)'}}>
-        <a style={s.name}>{props.entry.name}</a>
+        {/* <a style={s.name}>{props.entry.name}</a> */}
+        <a style={s.duration}>
+          {`${hours}h ${minutes}m ${seconds}s`}
+        </a>
         <a style={s.time}>
           {moment(parseInt(props.entry.timestamp_start)).format(`h:mm A`)} - {moment(parseInt(props.entry.timestamp_end)).format(`h:mm A`)}
         </a>
