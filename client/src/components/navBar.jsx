8fdbafc3f42ec2e011c91ac126fea6690412447e
EthanVV <ethan.vanvleck@gmail.com>
2018-11-28 14:29:43 -0600

    fix navbar overflow

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -4,43 +4,33 @@ import DevSimB from '../components/devSimB.jsx';
 const s = {
   wrap: {
    display: 'grid',
-   gridTemplateRows: '100px 100px 100px 100px 1fr 100px',
+   gridTemplateRows: '100px 100px 100px 100px 1fr',
    height: '100%',
-   padding: '2px',
    gridGap: '2px',
-   overflow: 'hidden'
+   padding: '2px',
+   overflow: 'wrap'
   },
   item: {
     backgroundColor: 'lightGrey',
     textAlign: 'center',
     fontSize: 30,
     fontStyle: 'bold',
-  },
-  account: {
-    gridRow: '-1',
-    fontSize: 12,
-    color: 'white'
   }
 }
 
 const NavBar = (props) => (
   <div style={s.wrap}>
-    <div style={s.item} onClick={() => props.changeView('mainView')}>Main</div>
-    <div style={s.item} onClick={() => props.changeView('historyView')}>Hist</div>
-    <div style={s.item}>C</div>
-    <div style={s.item} onClick={() => props.changeView('settingsView')}>Set</div>
-    <div>
-      <DevSimB
-        getActInfo={props.getActInfo}
-        assignedActivities={props.assignedActivities}
-        taskChange={props.taskChange}
-      />
-    </div>
-    <div style={s.account}>
-      <p>Dev Info</p>
-      <p>{props.account.email}</p>
-      <p>{props.account.id}</p>
-    </div>
+      <div style={s.item} onClick={() => props.changeView('mainView')}>Main</div>
+      <div style={s.item} onClick={() => props.changeView('historyView')}>Hist</div>
+      <div style={s.item}>C</div>
+      <div style={s.item} onClick={() => props.changeView('settingsView')}>Set</div>
+      <div>
+        <DevSimB
+          getActInfo={props.getActInfo}
+          assignedActivities={props.assignedActivities}
+          taskChange={props.taskChange}
+        />
+      </div>
   </div>
 )
 
