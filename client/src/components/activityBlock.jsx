55468677cf81c55fe4f9f3cfd99d84f2f4b1c99f
EthanVV <ethan.vanvleck@gmail.com>
2018-11-30 17:23:19 -0600

    rename activityButton to activityBlock

--- client/src/components/activityBlock.jsx
+++ client/src/components/activityBlock.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const s = {
 }
 
-const ActivityButton = (props) => (
+const ActivityBlock = (props) => (
   <div style={{backgroundColor: props.info.color}} onClick={()=>props.clickEvent(props.id)}>
     <a>{props.info.name}</a>
   </div>
 )
 
-export default ActivityButton;
+export default ActivityBlock;
---
+++ client/src/components/trackerView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CurrentActivityBlock from './currentActivityBlock.jsx';
+
+const TrackerView = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default TrackerView;
